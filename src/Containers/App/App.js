c8ec5928a35248b5415254057507552031707584
Mouhamadou <mouhamadoul.sarr@gmail.com>
2023-09-23 18:13:43 +0200

    update affichage article avec useparams

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -10,29 +10,21 @@ import Articles from "../Articles/Articles";
 import Article from "../Articles/Article/Article";
 
 function App() {
-	return (
-		<div className="App">
-			<Layout>
-				<Routes>
-					<Route exact path="/" element={<Home />} />
+  return (
+    <div className="App">
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
 
-					<Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/contact" element={<Contact />} />
 
-					<Route
-						exact
-						path="/articles"
-						element={<Articles />}
-					/>
+          <Route exact path="/articles" element={<Articles />} />
 
-					<Route
-						exact
-						path="/articles/:id"
-						element={<Article />}
-					/>
-				</Routes>
-			</Layout>
-		</div>
-	);
+          <Route exact path="/articles/:id" element={<Article />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
